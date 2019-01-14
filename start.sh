7be36d6c791624dd56d8f6c0bbd1c4a0c47de7f7
limingmax <limingmax@163.com>
2019-01-14 17:28:59 +0800

    krb5忘记修改

--- start.sh
+++ start.sh
@@ -12,4 +12,8 @@ sed -i "s/PRODUCER_TOPIC/${PRODUCER_TOPIC}/g" python.ini
 
 sed -i "s/REDIS_IP:REDIS_PORT/${REDIS_IP}:${REDIS_PORT}/g" python.ini
 
+sed -i "s/REALMS_KDC/${REALMS_KDC}/g" /etc/krb5.conf
+
+sed -i "s/REALMS_ADMIN_SERVER/${REALMS_ADMIN_SERVER}/g" /etc/krb5.conf
+
 python predict.py
