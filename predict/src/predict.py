4020865dbef4bdff961dfb0e63ee8cf16b44dbee
limingmax <limingmax@163.com>
2019-01-15 17:16:42 +0800

    1111

--- predict/src/predict.py
+++ predict/src/predict.py
@@ -82,8 +82,8 @@ def select( metric, resource, year, month, day, namespace):
     # hbase_host = '192.168.195.1'
     # hbase_port = 9090
     os.system('kinit -kt /etc/hbase.keytab hbase')
-    sock = TSocket.TSocket("k8s-alpha-master", 9090)
-    transport = TTransport.TSaslClientTransport(sock, "k8s-alpha-master", "hbase")
+    sock = TSocket.TSocket(hbase_host, hbase_port)
+    transport = TTransport.TSaslClientTransport(sock, hbase_host, "hbase")
     # Use the Binary protocol (must match your Thrift server's expected protocol)
     protocol = TBinaryProtocol.TBinaryProtocol(transport)
     client = Hbase.Client(protocol)
